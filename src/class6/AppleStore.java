f6e5fe3729accf2bbcd73c2e8ef1635a33a8f6ed
ChuHsin <37835079+ChuHsin@users.noreply.github.com>
2020-02-19 10:05:46 -0800

    add some understanding about this class note
    #interface #implements #Comparable #HashMap #Override

--- src/class6/AppleStore.java
+++ src/class6/AppleStore.java
@@ -1,35 +1,42 @@
 package class6;
 
-public interface AppleStore {
+public interface AppleStore { // 创建 AppleStore 接口
 
-	public iPhone buyPhone();
+	public iPhone buyPhone(); // 接口方法
 
-	public void returnPhone(iPhone phone);
+	public void returnPhone(iPhone phone); //接口方法
 
-	public int getNumberOfPhones();
+	public int getNumberOfPhones(); //接口方法
 
-	public void addPhones(int numberOfPhones);
+	public void addPhones(int numberOfPhones); // 接口方法
 
-	public int getTotalSales();
+	public int getTotalSales(); //接口方法
 
-	public default iPhone makePhone(int i) {
+	public default iPhone makePhone(int i) { // 在 interface 直接编写 implement 方法，Java8 新特性 ？？
 		return iPhone.makePhone(i);
 	}
-
 }
 
-class iPhone implements Comparable<iPhone> {
-	float price;
+class iPhone implements Comparable<iPhone> { 
+	/**
+	For any class to support natural ordering, 
+	it should implement the Comparable interface and override it’s compareTo() method. 
+	It must return a negative integer, zero, or a positive integer as this object is 
+	less than, equal to, or greater than the specified object.
+	https://howtodoinjava.com/java/collections/java-comparable-interface/
+	*/
+	float price; // initializing field
 	String number;
 
-	public static iPhone makePhone(int i) {
+	public static iPhone makePhone(int i) { // 创建 iPhone 类的方法
 		iPhone p = new iPhone();
 		p.price = 1099;
 		p.number = i + "";
 		return p;
 	}
 
-	public boolean equals(Object o) {
+	public boolean equals(Object o) { // 重写 equals 方法用于比较这一类，供 hashMap 比较使用（hashMap) 要重写 equals and hashCode
+		//hashMap 中的 key 值不能重复 先比较 hashCode 再看是否相等，都不同才能加入
 		iPhone p = (iPhone) o;
 		if (p.number.equals(this.number)) {
 			return true;
@@ -41,12 +48,12 @@ class iPhone implements Comparable<iPhone> {
 		return number.hashCode();
 	}
 
-	public String toString() {
-		return "[Number,Price]=[" + number + "," + price + "]";
+	public String toString() { // 重写 toString
+		return "[Number,Price]=[" + n umber + "," + price + "]";
 	}
 
 	@Override
-	public int compareTo(iPhone o) {
+	public int compareTo(iPhone o) { // 重写 compareTo 方法
 		// return this.number.compareTo(o.number);
 		return Integer.parseInt(number) - Integer.parseInt(o.number);
 	}
